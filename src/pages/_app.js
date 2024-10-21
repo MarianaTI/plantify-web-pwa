0fd9fd22470b849aff2cf0a457aa29c23e8b3dbd
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2024-10-20 19:51:58 -0600

    pwa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,20 @@ import usePullToRefresh from "@/components/PullRefresh";
 import { PageContentProvider } from "@/context/PageContentContext";
 import ResponsiveDrawer from "@/layout";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   usePullToRefresh();
-  return  (
+
+  return (
     <PageContentProvider>
+      <Head>
+        <title>Plantify Web App</title>
+        <meta name="description" content="Plantify PWA" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#fff" />
+        <link rel="apple-touch-icon" href="/icon512_rounded.png" />
+      </Head>
       <ResponsiveDrawer>
         <Component {...pageProps} />
       </ResponsiveDrawer>
